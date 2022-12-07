d7e8ee43b5877cc997343f454c43e520bc079955
JakubVondra <jakubvond@seznam.cz>
2022-12-07 15:20:17 +0100

    Přidány styly do nabidka.css, upraven soubor nabidka.php, index.php, a styly.css přejmenovány na landing.css. Přidány tři nové obrázky do nabidky.

--- nabidka.php
+++ nabidka.php
@@ -5,12 +5,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styly.css">
+    <link rel="stylesheet" href="nabidka.css">
     <title>Nabídka World of Burgers</title>
 </head>
 
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#" class="Home">World of Burgers</a></li>
+                <li><a href="#" class="Menu">Nabídka</a></li>
+                <li><a href="#" class="Contact">Kontakt</a></a></li>
+            </ul>
+        </nav>
+        <div class="header-text">
+            <h1>Nabídka našich burgrů</h1>
+        </div>
+    </header>
 
+    <main>
+        <div class="menu-1">
+            <img src="img/big_cheese1.jpg" alt="Big Cheesburger">
+        </div>
+        <div class="Nadpis1">
+            <h2>Big Cheesburger</h2>
+        </div>
+        <div class="menu-2">
+            <img src="img/special_burger1.jpg" alt="Special Burger">
+        </div>
+        <div class="Nadpis2">
+            <h2>Special Burger</h2>
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
 </body>
 
 </html>
