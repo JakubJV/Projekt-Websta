dc502a6850cb06356c200e5b8a522291d3be2498
JakubVondra <jakubvond@seznam.cz>
2022-12-09 13:08:30 +0100

    Dodělaná stylizace landing.css a nabidka.css, přidán text do patičky v index.php a nabidka.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="landing.css">
+    <link rel="stylesheet" href="landing.css" />
     <title>World of Burgers</title>
 </head>
 
@@ -32,7 +32,9 @@
     </main>
 
     <footer>
-
+        <p>
+            Created by World of Burgers. © 2022
+        </p>
     </footer>
 </body>
 
--- nabidka.php
+++ nabidka.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nabidka.css">
+    <link rel="stylesheet" href="nabidka.css" />
     <title>Nabídka World of Burgers</title>
 </head>
 
@@ -26,20 +26,22 @@
     <main>
         <div class="menu-1">
             <img src="img/big_cheese1.jpg" alt="Big Cheesburger">
-        </div>
-        <div class="Nadpis1">
             <h2>Big Cheesburger</h2>
+            <p>pořádná nálož hovězího s plátky cheddaru mezi dvěma kusy zlatavé bulky
+            </p>
         </div>
         <div class="menu-2">
             <img src="img/special_burger1.jpg" alt="Special Burger">
-        </div>
-        <div class="Nadpis2">
             <h2>Special Burger</h2>
+            <p>burger s kuřecím plátkem masa pokrytý avokádovou pastou</p>
         </div>
+        <div class="clearfix"></div>
     </main>
 
     <footer>
-
+        <p>
+            Created by World of Burgers. © 2022
+        </p>
     </footer>
 </body>
 
