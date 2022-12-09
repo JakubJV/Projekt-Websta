9ca6eb6756becc79a033906e29aaa5e693a096f7
JakubVondra <jakubvond@seznam.cz>
2022-12-09 14:35:12 +0100

    Upraveny soubory index.php, nabidka.php, nabidka.css, přidán kontakt.php a kontakt.css

--- kontakt.php
+++ kontakt.php
@@ -5,12 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="landing.css" />
-    <title>World of Burgers</title>
+    <link rel="stylesheet" href="kontakt.css" />
+    <title>Kontakt</title>
 </head>
 
 <body>
-
     <header>
         <nav>
             <ul>
@@ -19,23 +18,28 @@
                 <li><a href="#" class="Contact">Kontakt</a></a></li>
             </ul>
         </nav>
-        <div class="clearfix"></div>
-        <div class="header-text">
-            <h1>World of Burgers</h1>
-        </div>
-
-
     </header>
 
-    <main>
+    <div class="banner"></div>
 
+    <main>
+        <div class="container">
+            <h1>World of Burgers</h1>
+            <div class="row">
+                <div class="column">
+                    <p>Pražská 2380
+                        <br>
+                        Hradec Králové
+                    </p>
+                    <p>
+                        Telefon: <strong style="font-size: 120%;">+420 600 350 250</strong>
+                    </p>
     </main>
 
     <footer>
-        <p>
-            Created by World of Burgers. © 2022
-        </p>
+
     </footer>
+
 </body>
 
 </html>
