016fca9d84500b7f17c5e6cdf21f49ddd9bdd393
JakubVondra <jakubvond@seznam.cz>
2022-12-10 15:54:13 +0100

    Všechny css soubory přidány do samostatné složky CSS, upravena barva písma pod položkami v nabidka.css

--- nabidka.php
+++ nabidka.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nabidka.css" />
+    <link rel="stylesheet" href="CSS/nabidka.css" />
     <title>Nabídka World of Burgers</title>
 </head>
 
@@ -13,7 +13,7 @@
     <header>
         <nav>
             <ul>
-                <li><a href="index.php" class="Home">World of Burgers</a></li>
+                <li><a href=" index.php" class="Home">World of Burgers</a></li>
                 <li><a href="nabidka.php" class="Menu">Nabídka</a></li>
                 <li><a href="kontakt.php" class="Contact">Kontakt</a></a></li>
             </ul>
