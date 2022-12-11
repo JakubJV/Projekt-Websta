c256b27a841de0c3d5d8956d0be368929bb7a168
JakubVondra <jakubvond@seznam.cz>
2022-12-11 12:50:28 +0100

    stylizace nabidka.css přetvořena a dokončena, v landing.css změněna barva nav

--- nabidka.php
+++ nabidka.php
@@ -13,7 +13,7 @@
     <header>
         <nav>
             <ul>
-                <li><a href=" index.php" class="Home">World of Burgers</a></li>
+                <li><a href="index.php" class="Home">World of Burgers</a></li>
                 <li><a href="nabidka.php" class="Menu">Nabídka</a></li>
                 <li><a href="kontakt.php" class="Contact">Kontakt</a></a></li>
             </ul>
@@ -23,8 +23,25 @@
     <div class="banner"></div>
 
     <main>
+        <div class="container">
+            <h1>Nabídka World of Burgers</h1>
+            <div class="row">
+                <div class="column1">
+                    <img src="CSS/img/big_cheese1.jpg" />
+                    <h3>Big Cheese</h3>
+                    <p>Pořádná nálož hovězího s plátky cheddaru mezi dvěma kusy zlatavé bulky
+                    </p><br>
+                    <p>(pšeničná houska, 200g čerstvého hovězího masa, cheddar, hermelín, kyselá okurka, BBQ omáčka)</p>
+                </div>
+                <div class="column2">
+                    <img src="CSS/img/special_burger1.jpg" />
+                    <h3>Special Burger</h3>
+                    <p>Burger s kuřecím plátkem masa pokrytým avokádovou pastou</p><br>
+                    <p>(pšeničná houska, 200g kuřecího masa, cheddar, avokádová pasta, kyselá okurka, BBQ omáčka)</p>
 
+                </div>
     </main>
+
     <?php
 
     include "footer.php";
