1e7d459ed2c7c986ea35045301896deaef722b8b
JakubVondra <jakubvond@seznam.cz>
2022-12-14 11:15:55 +0100

    Z nabidka.php vyjmut formulář do samostatného souboru formular.php a k tomu vytvořeny styl ve formular.css

---
+++ formular.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/nabidka.css">
+    <title></title>
+</head>
+<body>
+
+    <div class="column3">
+        <form action="" class="form">
+            <h5>Napište jak Vám chutnalo</h5>
+            <input type="text" placeholder="Jméno a příjmení">
+            <input type="email" placeholder="Email">
+            <textarea name="" rows="10" cols="30"></textarea>
+            <input type="submit" value="Odeslat">
+        </form>
+    </div>
+    
+</body>
+</html>
